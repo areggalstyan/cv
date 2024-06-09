c60129a0e01a818a48d15563d95181ce588059fa
areggalstyan <areg.galstyan.levon@gmail.com>
2024-06-09 18:21:20 +0400

    Replace # with #/ in TopBar.svelte

--- src/lib/TopBar.svelte
+++ src/lib/TopBar.svelte
@@ -7,7 +7,7 @@
 
 <div class='p-5 border-b-2 border-secondary'>
   <h1 class='text-4xl font-black'>
-    <a href='#'><InlineFa icon={faArrowLeft} /></a>
+    <a href='#/'><InlineFa icon={faArrowLeft} /></a>
     {title}
   </h1>
 </div>
